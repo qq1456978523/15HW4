de3c28102c36b0f62b537de1b7f09bb8792f720a
c8610290927 <c8610290927@gmail.com>
2020-01-13 15:01:15 +0800

    Initial Commit

---
+++ 15HW4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  15HW4
+//
+//  Created by User20 on 2020/1/13.
+//  Copyright © 2020 000. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
